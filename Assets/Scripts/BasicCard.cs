12f54f6b90e317b6ff06d8e70ca25f6d98a8f9db
elequestrian <61123120+elequestrian@users.noreply.github.com>
2023-02-13 08:31:49 -0500

    Finish Card InfoPanel Implementation
    Added a call to display the card on click to BasicCard and made InfoPanel a singleton.

--- Assets/Scripts/BasicCard.cs
+++ Assets/Scripts/BasicCard.cs
@@ -13,16 +13,18 @@ namespace Com.SakuraStudios.FECipherPlayer
 
         #endregion
 
+
         #region Properties
 
         public Sprite CardSprite { get { return cardSprite; } }             // Returns the cardSprite for other classes.
 
         #endregion
 
+
         #region Protected Functions
 
         // This method takes the place of the Start method as Start() is not called in abstract classes.
-        // This method must be called on all inherited instances of this class
+        // NOTE: This method must be called on all inherited instances of this class
         protected virtual void SetUpCard()
         {
             SpriteRenderer renderer = GetComponent<SpriteRenderer>();
@@ -37,6 +39,9 @@ namespace Com.SakuraStudios.FECipherPlayer
 
         #endregion
 
+
+        #region IPointerHandler Interface Implementation
+
         // 
         public void OnPointerEnter(PointerEventData pointerEventData)
         {
@@ -49,11 +54,15 @@ namespace Com.SakuraStudios.FECipherPlayer
             Debug.Log("Cursor Exiting " + name + " GameObject");
         }
 
-        // 
+        // When a card is clicked it should display it's translation and sprite in the Info Panel.
         // Note: Requires a physics collider on the gameObject and a Physics Raycaster on the camera in order to be called.
         public void OnPointerClick(PointerEventData pointerEventData)
         {
-
+            InfoPanelController.Instance.DisplayCard(this);
         }
+
+        #endregion
+
+
     }
 }
--- Assets/Scripts/InfoPanelController.cs
+++ Assets/Scripts/InfoPanelController.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 namespace Com.SakuraStudios.FECipherPlayer
 {
-
+    // This class controls the functions of the InfoPanel to display card information.
     public class InfoPanelController : MonoBehaviour
     {
-        public UnityEngine.UI.Image displayImage;             //The location we're placing the image.
+        public static InfoPanelController Instance = null;                      // The instance reference to this script as it is a singleton pattern.
+        
+        [SerializeField] private UnityEngine.UI.Image displayImage;             // The location we're placing the image.
+
+
+        // Awake is always called before any Start functions
+        void Awake()
+        {
+            //Check if instance already exists
+            if (Instance == null)
+
+                //if not, set instance to this
+                Instance = this;
+
+            //If instance already exists and it's not this:
+            else if (Instance != this)
+
+                //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of the InfoPanelController.
+                Destroy(gameObject);
+        }
 
         // Start is called before the first frame update
         void Start()
@@ -21,7 +40,7 @@ namespace Com.SakuraStudios.FECipherPlayer
 
         }
 
-        // 
+        // Method called by a BasicCard when clicked to display the card information. 
         public void DisplayCard(BasicCard card)
         {
             displayImage.sprite = card.CardSprite;
