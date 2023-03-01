8b36a75b33609c0d44a499fd3400ca3cb4b7f596
elequestrian <61123120+elequestrian@users.noreply.github.com>
2023-02-28 22:05:51 -0500

    Adding Music
    Added music and starting on the path to better card visuals.

---
+++ Assets/Scripts/CardVisualController.cs
@@ -0,0 +1,34 @@
+using Com.SakuraStudios.FECipherPlayer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardVisualController : MonoBehaviour
+{
+    [SerializeField] private float tapTime = 0.5f;
+    [SerializeField] private float flipTime = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* Will likely handle card movement in animator again.
+    public IEnumerator TapCardObject()
+    {
+        float tapTimer = 0f;
+        
+        while (tapTimer < tapTime)
+        {
+            transform.ro
+        }
+    }
+    */
+}
